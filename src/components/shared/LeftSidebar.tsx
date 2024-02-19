af00027f43e4b47d3018e6644bd109714c465807
dmkur <fgameua@gmail.com>
2024-02-19 21:54:17 +0200

    Working on LeftSidebar modified:   src/components/shared/LeftSidebar.tsx new file:   src/constants/index.ts

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { Link, NavLink, useNavigate,useLocation } from "react-router-dom";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
 
 const LeftsideBar = () => {
-  return (
-    <div>LeftsideBar</div>
-  )
-}
+  const {pathname}= useLocation()
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
 
-export default LeftsideBar
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+        <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+          <img
+            src={user.imageUrl || "/assets/images/profile.png"}
+            alt="profile"
+            className="w-14 h-14 rounded-full"
+          />
+
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route
+
+            return (
+              <li className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`} key={link.label}>
+                <NavLink
+                  to={link.route}
+                  className={"flex items-center gap-4 p-4"}
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${isActive && "invert-white"}`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default LeftsideBar;
