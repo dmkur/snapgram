e87f567540c38d46812ee5f354b258ddac037eac
dmkur <fgameua@gmail.com>
2024-02-21 20:28:04 +0200

    starting workin on FileUploader Changes to be committed: modified:   package-lock.json modified:   package.json modified:   src/components/forms/PostForm.tsx modified:   src/components/shared/FileUploader.tsx

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+import { Button } from "../ui/button";
 
-const FileUploader = () => {
+type FileUploaderProps = {
+  fieldChange: (FILES: File[]) => void;
+  mediaUrl: string;
+};
+
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState("");
+  // What hapends when we actualy drop some files
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      // Do something with the files
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+    },
+    [file]
+  );
+  // Define wwhat we can use
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".jpeg", ".png", ".jpg", ".svg"],
+    },
+  });
   return (
-    <div>FileUploader</div>
-  )
-}
+    <div
+      {...getRootProps()}
+      className="flex flex-col flex-center bg-dark-3 rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <div className="">test 1 </div>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={96}
+            height={77}
+            alt="file-upload"
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">Drag photo her</h3>
+          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPEG</p>
 
-export default FileUploader
+          <Button className="shad-button_dark_4 bg-black">
+            Select from PC
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
